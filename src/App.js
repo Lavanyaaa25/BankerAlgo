8f4b4a83599de2734b6ee0fd50f114b699109398
chandushree-hs <115502458+chandushree-hs@users.noreply.github.com>
2023-11-01 23:59:44 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -79,12 +79,18 @@ function App() {
       )}
       <AvailableMatrix allocationMatrix={allocationMatrix} processCount={processCount} instanceCount={instanceCount} />
 
-      <button class="flex sm:inline-flex justify-center items-center bg-gradient-to-tr from-pink-500 to-red-400 hover:from-pink-600 hover:to-red-500 active:from-pink-700 active:to-red-600 focus-visible:ring ring-pink-300 text-white font-semibold text-center rounded-md outline-none transition duration-100 px-5 py-2" onClick={calculateNeedMatrix}>Calculate Need</button>
-      <SequenceComp />
+      <button class="flex sm:inline-flex justify-center items-center bg-gradient-to-tr from-pink-600 to-red-500 hover:from-pink-500 hover:to-red-400 active:from-pink-700 active:to-red-600 focus-visible:ring ring-pink-300 text-white font-semibold text-center rounded-md outline-none transition duration-100 px-5 py-2" onClick={calculateNeedMatrix}>Calculate Need</button>
+      <SequenceComp
+        allocationMatrix={allocationMatrix}
+        maximumMatrix={maximumMatrix}
+        instanceCount={instanceCount}
+      />
     </div>
   );
 }
 
 export default App;
+
+
 
 
