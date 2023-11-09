f21ddc8c8d8bf0f5b2d3a059bc91c2452a769f2f
Lavanya <slavanya2509@gmail.com>
2023-11-09 19:06:16 +0530

    available fixed

--- src/App.js
+++ src/App.js
@@ -6,33 +6,34 @@ import Counter from './Counter';
 import NeedMatrix from './needMatrix';
 import Available from './Available';
 
+// ... (existing imports)
 
 function App() {
   const [processCount, setProcessCount] = useState(5);
-  const [instances, setInstances] = useState();
+  const [instances, setInstances] = useState([0, 0, 0]); // Initialize with zeros
   const [allocationMatrix, setAllocationMatrix] = useState([]);
   const [maximumMatrix, setMaximumMatrix] = useState([]);
   const [availableMatrix, setAvailableMatrix] = useState([]);
   const [showNeedMatrix, setShowNeedMatrix] = useState(false);
   const [showAvailableMatrix, setShowAvailableMatrix] = useState(false);
-  
+
   const handleProcessCountChange = (newCount) => {
     setProcessCount(newCount);
   };
+
   const calculateAvailableMatrix = () => {
-    const totalInstances = [10,10,10]; // Replace with actual values
     const allocatedSum = Array(3).fill(0);
-  
+
     // Calculate the sum of allocated resources for each process
     for (let i = 0; i < processCount; i++) {
       for (let j = 0; j < 3; j++) {
         allocatedSum[j] += parseInt(allocationMatrix[i][j]) || 0;
       }
     }
-  
+
     // Subtract the allocated sum from total instances to get available resources
-    const availableMatrix = totalInstances.map((total, index) => total - allocatedSum[index]);
-  
+    const availableMatrix = instances.map((total, index) => total - allocatedSum[index]);
+
     setAvailableMatrix(availableMatrix);
   };
 
@@ -40,6 +41,7 @@ function App() {
     calculateAvailableMatrix(); // Calculate the available matrix before showing it
     setShowAvailableMatrix(!showAvailableMatrix);
   };
+
   const initializeMatrices = (count) => {
     const initialAllocationMatrix = Array.from({ length: count }, () => Array(3).fill(''));
     const initialMaximumMatrix = Array.from({ length: count }, () => Array(3).fill(''));
@@ -81,35 +83,30 @@ function App() {
     }
   };
 
-
-
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold mb-4">Banker's Algo Simulator</h1>
-      
+
       <div className="mb-4">
         <Counter name="No of processes" onCountChange={handleProcessCountChange} />
       </div>
       <br></br>
-      <InputTable title="Instances" processCount="1" sub="0" onDataChange={setInstances}/>
-    <br></br>
-      <InputTable title="Allocation" processCount={processCount} onDataChange={setAllocationMatrix} sub="1"/>
-      <InputTable title="Maximum" processCount={processCount} onDataChange={setMaximumMatrix} sub="1"/>
+      <InputTable title="Instances" processCount="1" sub="0" onDataChange={(data) => setInstances(data[0])} />
+      <br></br>
+      <InputTable title="Allocation" processCount={processCount} onDataChange={setAllocationMatrix} sub="1" />
+      <InputTable title="Maximum" processCount={processCount} onDataChange={setMaximumMatrix} sub="1" />
 
       {showNeedMatrix && (
         <NeedMatrix allocationMatrix={allocationMatrix} maximumMatrix={maximumMatrix} />
       )}
 
-     
-
       <div className="button-container">
-      <button
-  className="flex sm:inline-flex justify-center items-center bg-gradient-to-tr from-pink-600 to-red-500 hover:from-pink-500 hover:to-red-400 active:from-pink-700 active:to-red-600 focus-visible:ring ring-pink-300 text-white font-semibold text-center rounded-md outline-none transition duration-100 px-5 py-2 calculate-button"
-  onClick={calculateNeedMatrix}
->
-  Calculate Need
-</button>
-
+        <button
+          className="flex sm:inline-flex justify-center items-center bg-gradient-to-tr from-pink-600 to-red-500 hover:from-pink-500 hover:to-red-400 active:from-pink-700 active:to-red-600 focus-visible:ring ring-pink-300 text-white font-semibold text-center rounded-md outline-none transition duration-100 px-5 py-2 calculate-button"
+          onClick={calculateNeedMatrix}
+        >
+          Calculate Need
+        </button>
       </div>
       <div className="button-container">
         <button
@@ -128,3 +125,4 @@ function App() {
 }
 
 export default App;
+
